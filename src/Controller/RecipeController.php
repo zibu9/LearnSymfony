1fa4fba5f1d1d39c93e1e73e48501c9954b49ac1
Zibu <kabuyajunior082@gmail.com>
2024-02-19 13:58:53 +0100

    any update

--- src/Controller/RecipeController.php
+++ src/Controller/RecipeController.php
@@ -3,14 +3,16 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class RecipeController extends AbstractController
 {
-    #[Route('/recipe', name: 'app_recipe')]
-    public function index(): Response
+    #[Route('/recette/{slug}-{id}', name: 'recipe.show')]
+    public function index(Request $request): Response
     {
+        dd($request);
         return $this->render('recipe/index.html.twig', [
             'controller_name' => 'RecipeController',
         ]);
