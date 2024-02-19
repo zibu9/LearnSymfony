d58b8b5f640977782a5c543945dce7b47a462352
Zibu <kabuyajunior082@gmail.com>
2024-02-19 13:11:56 +0100

    add recipe controller

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,14 +2,16 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'home')]
-    public function index(): Response{
-        return new Response('Hello World');
+    public function index(Request $request): Response{
+        return new Response('Hello ' . $request->query->get('name', 'Inconnu'));
     }
 }
---
+++ src/Controller/RecipeController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class RecipeController extends AbstractController
+{
+    #[Route('/recipe', name: 'app_recipe')]
+    public function index(): Response
+    {
+        return $this->render('recipe/index.html.twig', [
+            'controller_name' => 'RecipeController',
+        ]);
+    }
+}
