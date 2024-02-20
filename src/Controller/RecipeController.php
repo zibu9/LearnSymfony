b80c9f56afee5ff1a0c3b54ddc6561620c17d5aa
Zibu <kabuyajunior082@gmail.com>
2024-02-20 13:43:05 +0100

    learn tags and filters

--- src/Controller/RecipeController.php
+++ src/Controller/RecipeController.php
@@ -12,17 +12,19 @@ class RecipeController extends AbstractController
     #[Route('/recette', name: 'recipe.index')]
     public function index(Request $request): Response
     {
-        return $this->render('recipe/index.html.twig', [
-            'controller_name' => 'RecipeController',
-        ]);
+        return $this->render('recipe/index.html.twig');
     }
 
     #[Route('/recette/{slug}-{id}', name: 'recipe.show', requirements: ['id' => '\d+', 'slug' => '[a-z0-9-]+'])]
-    public function show(Request $request): Response
+    public function show(Request $request, string $slug, int $id): Response
     {
-        dd($request);
-        return $this->render('recipe/index.html.twig', [
-            'controller_name' => 'RecipeController',
+        return $this->render('recipe/show.html.twig', [
+            'slug' => $slug,
+            'id' => $id,
+            'person' => [
+                'name' => 'Jeanne',
+                'age' => 62,
+            ],
         ]);
     }
 }
