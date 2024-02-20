447895ae7be24989c0312d51c960ccdc1f1da3e8
Zibu <kabuyajunior082@gmail.com>
2024-02-20 11:33:44 +0100

    add requirement to url

--- src/Controller/RecipeController.php
+++ src/Controller/RecipeController.php
@@ -9,7 +9,7 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class RecipeController extends AbstractController
 {
-    #[Route('/recette/{slug}-{id}', name: 'recipe.show')]
+    #[Route('/recette/{slug}-{id}', name: 'recipe.show', requirements: ['id' => '\d+', 'slug' => '[a-z0-9-]+'])]
     public function index(Request $request): Response
     {
         dd($request);
