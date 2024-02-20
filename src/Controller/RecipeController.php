02a04923348708d6b6d58f5f001834e4a427c85e
Zibu <kabuyajunior082@gmail.com>
2024-02-20 12:07:51 +0100

    template twing learning

--- src/Controller/RecipeController.php
+++ src/Controller/RecipeController.php
@@ -9,8 +9,16 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class RecipeController extends AbstractController
 {
-    #[Route('/recette/{slug}-{id}', name: 'recipe.show', requirements: ['id' => '\d+', 'slug' => '[a-z0-9-]+'])]
+    #[Route('/recette', name: 'recipe.index')]
     public function index(Request $request): Response
+    {
+        return $this->render('recipe/index.html.twig', [
+            'controller_name' => 'RecipeController',
+        ]);
+    }
+
+    #[Route('/recette/{slug}-{id}', name: 'recipe.show', requirements: ['id' => '\d+', 'slug' => '[a-z0-9-]+'])]
+    public function show(Request $request): Response
     {
         dd($request);
         return $this->render('recipe/index.html.twig', [
