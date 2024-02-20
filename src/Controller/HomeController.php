4b4e0f432c5585eb50b0297b5dd2e8747003e353
Zibu <kabuyajunior082@gmail.com>
2024-02-20 13:51:05 +0100

    change index link

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -12,6 +12,6 @@ class HomeController extends AbstractController
 {
     #[Route('/', name: 'home')]
     public function index(Request $request): Response{
-        return new Response('Hello ' . $request->query->get('name', 'Inconnu'));
+        return $this->render('home/index.html.twig');
     }
 }
